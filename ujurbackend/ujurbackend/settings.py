ed42c145ab9b665e76726daae4afa6adcc121b4f
vohraaYatinn <vohrayatin9@gmail.com>
2024-04-04 19:28:47 +0530

    ss

--- ujurbackend/ujurbackend/settings.py
+++ ujurbackend/ujurbackend/settings.py
@@ -149,7 +149,7 @@ CSRF_TRUSTED_ORIGINS = [    'http://localhost:3000', 'http://localhost:3001', "h
 "http://91.108.110.30:3000"
 
 ]
-CORS_ORIGIN_WHITELIST = [    'http://localhost:3000', 'http://localhost:3001' ,  "http://127.0.0.1:3000", 'http://91.108.110.87:3000',
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'http://localhost:3001' ,  "http://127.0.0.1:3000", 'http://91.108.110.87:3000',
 'http://91.108.110.87:3001',
 'http://91.108.110.87:3002',
 "http://91.108.110.30:3000"
